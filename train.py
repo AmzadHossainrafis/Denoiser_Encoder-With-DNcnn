45f8fa049f4767e04207c9a5fdc47adeb79b1a76
Amzad hossain <amzad.rafi@northsouth.edu>
2022-05-21 22:14:03 +0600

    config implimented

--- train.py
+++ train.py
@@ -6,17 +6,17 @@ from dataloader import DataLoader
 from utils import download_data, plot_loss, SelectCallbacks,read_config
 
 
-x_train, x_test = download_data()
+x_train, x_test = download_data()                           # download data
 
 
-config=read_config()
-data_loader = DataLoader(x_train=x_train,batch_size=256)
-val_loader = DataLoader(x_train=x_test,batch_size=256)
+config=read_config()                                        #import config 
+data_loader = DataLoader(x_train=x_train,batch_size=256)    #import data loader
+val_loader = DataLoader(x_train=x_test,batch_size=256)      #import validation data loader
 
-call_backs = SelectCallbacks()
+call_backs = SelectCallbacks()                              #import callbacks
 callbacks=call_backs.get_callbacks()
 
-model=DnCNN()
+model=DnCNN()                                              #import model
 model.compile(optimizer=config['optimizer'], loss=config['loss'], metrics=['mse'])
 history1=model.fit(data_loader, epochs=config['epochs'], validation_data=val_loader,callbacks=callbacks,shuffle=config['shuffle'])
 
